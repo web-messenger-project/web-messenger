e763dc15d1d47f98b1a2df724686c36527e31f22
Yindaoliuxue <tobiasz.wesolowski2007@gmail.com>
2025-06-17 19:08:29 +0200

    barebones ui

---
+++ ui/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace LoginRegisterApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void lblRegister_Click(object sender, EventArgs e)
+        {
+            RegisterForm regForm = new RegisterForm();
+            regForm.ShowDialog();
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            string login = txtLogin.Text.Trim();
+            string password = txtPassword.Text;
+
+            if (string.IsNullOrEmpty(login) || string.IsNullOrEmpty(password))
+            {
+                MessageBox.Show("Please enter both login and password.", "Login Failed", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            if (login == "admin" && password == "admin")
+            {
+                MessageBox.Show("Login successful!", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                MessageBox.Show("Invalid login or password.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
---
+++ ui/RegisterForm.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace LoginRegisterApp
+{
+    public partial class RegisterForm : Form
+    {
+        public RegisterForm()
+        {
+            InitializeComponent();
+        }
+
+        private void btnRegister_Click(object sender, EventArgs e)
+        {
+            string info = $"Name: {txtName.Text}\n" +
+                          $"Surname: {txtSurname.Text}\n" +
+                          $"Email: {txtEmail.Text}\n" +
+                          $"Login: {txtLogin.Text}\n" +
+                          $"Password: {txtPassword.Text}";
+
+            MessageBox.Show(info, "Registered Info", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+            this.Close();
+        }
+    }
+}
