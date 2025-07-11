0015e305ca0741fbc72c485d993776007d2897da
Piotr Czarnecki <czarnecki.devv@gmail.com>
2025-07-11 19:38:07 +0200

    fix/delete unnecessary URL path for easier usage

--- API/userAPI/urls.py
+++ API/userAPI/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from userAPI import views
 
 urlpatterns = [
-    path('api/login/', views.login, name='login'),
-    path('api/register/', views.register, name='register'),
-    path('api/delete/', views.delete_user, name='delete_user'),
-    path('api/getall/', views.getAllUsers, name='getall')
+    path('login/', views.login, name='login'),
+    path('register/', views.register, name='register'),
+    path('delete/', views.delete_user, name='delete_user'),
+    path('getall/', views.getAllUsers, name='getall')
 ]
